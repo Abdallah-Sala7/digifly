b76343742fb981f1d8513e95e11cde2cd783e7a8
Abdallah-sala7 <Abdallah-Sala7@users.noreply.github.com>
2024-11-24 20:37:03 +0200

    fix fetch

--- api/mainApi.ts
+++ api/mainApi.ts
@@ -1,18 +1,25 @@
 const BASE_URL = "http://localhost:1337";
 
 export async function getAllUsers({ search }: { search?: string }) {
-  const res = await fetch(
-    `${BASE_URL}/user-informations${search ? `?FirstName=${search}` : ""}`,
-    {
-      cache: "no-store",
-      next: {
-        tags: ["users"],
-      },
-    }
-  );
+  try {
+    const res = await fetch(
+      `${BASE_URL}/user-informations${search ? `?FirstName=${search}` : ""}`,
+      {
+        cache: "no-store",
+        next: {
+          tags: ["users"],
+        },
+      }
+    );
 
-  return {
-    isSuccess: res.ok,
-    data: await res.json(),
-  };
+    return {
+      isSuccess: res.ok,
+      data: await res.json(),
+    };
+  } catch (error) {
+    return {
+      isSuccess: false,
+      data: error,
+    };
+  }
 }
